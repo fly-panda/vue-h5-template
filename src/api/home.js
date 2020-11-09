b6164e2870cd1edb2748a9498b3eb550a8dab7c8
fly <hdx_fly@163.com>
2020-11-09 15:05:04 +0800

    备注

--- src/api/home.js
+++ src/api/home.js
@@ -3,4 +3,10 @@
 // import request from '@/utils/request'
 //home api
 
- 
+// export function login(params) {
+//   return request({
+//     url: '/user/login',
+//     method: 'post',
+//     data: qs.stringify(params)
+//   })
+// }
